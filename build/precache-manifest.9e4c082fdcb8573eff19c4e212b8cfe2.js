16afe70c6492c5bd88a86e1dd2441dbcb7bf6d1f
jcleitonss <dev.si.jc@gmail.com>
2019-10-23 12:19:54 -0300

    first commit

---
+++ build/precache-manifest.9e4c082fdcb8573eff19c4e212b8cfe2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f0e7a702258323dbe676c66a7dfe9e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "347214dbb89e59c160fe",
+    "url": "/static/css/main.98c21220.chunk.css"
+  },
+  {
+    "revision": "00a7dca9304f47afd86c",
+    "url": "/static/js/2.76a45e29.chunk.js"
+  },
+  {
+    "revision": "347214dbb89e59c160fe",
+    "url": "/static/js/main.b3bae116.chunk.js"
+  },
+  {
+    "revision": "d653c3e1ff0c4fc20192",
+    "url": "/static/js/runtime-main.d01d61ca.js"
+  },
+  {
+    "revision": "8cedf4f3bc6e1cd72f0d3741e7d4788b",
+    "url": "/static/media/Consume.8cedf4f3.jpeg"
+  },
+  {
+    "revision": "2d909170072845bf4d906ddd7686c98e",
+    "url": "/static/media/news.2d909170.png"
+  },
+  {
+    "revision": "cdc6f2a906288dbaa3a84d86156f1af9",
+    "url": "/static/media/olho.cdc6f2a9.png"
+  }
+]);
